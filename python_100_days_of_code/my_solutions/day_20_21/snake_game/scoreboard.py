bb9d4b3539abc87267dfeee23a22bcb5450ad60d
Luan Rodrigues <luan15008090@gmail.com>
2026-09-22 19:41:34 -0300

    feat: add aulas 20 e 21 (snake game)

---
+++ python_100_days_of_code/my_solutions/day_20_21/snake_game/scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 20, "normal")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.color("white")
+        self.goto(0, 270)
+        self.score = 0
+        self.update_score()
+
+    def update_score(self):
+        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+            self.score += 1
+            self.clear()
+            self.update_score()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
